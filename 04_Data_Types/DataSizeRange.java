3e0a18c4716bd7a7642363388183e53d6be4acff
Ramireddy Chandra sekhar reddy <ramireddychandu77@gmail.com>
2022-11-29 22:37:33 +0530

    Java Data-types

---
+++ 04_Data_Types/DataSizeRange.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+
+
+class DataSizeRange
+{
+	public static void main(String args[])
+	{
+		// To get all such methods --->  javac java.lang.Integer(CMD);
+		
+		System.out.println("Short Min : "+ Short.MIN_VALUE);
+		System.out.println("Short Max : "+ Short.MAX_VALUE);
+		System.out.println("Short BYTES : "+ Short.BYTES);
+		System.out.println("*******************************");
+		System.out.println("int Min : "+ Integer.MIN_VALUE);
+		System.out.println("int Max : "+ Integer.MAX_VALUE);
+		System.out.println("int BYTES : "+ Integer.BYTES);
+		System.out.println("*******************************");
+		System.out.println("Float Min : "+ Float.MIN_VALUE);
+		System.out.println("Float Max : "+ Float.MAX_VALUE);
+		System.out.println("Float BYTES : "+ Float.BYTES);
+		System.out.println("*******************************");
+		System.out.println("Double Min : "+ Double.MIN_VALUE);
+		System.out.println("Double Max : "+ Double.MAX_VALUE);
+		System.out.println("Double BYTES : "+ Double.BYTES);
+	}
+}
