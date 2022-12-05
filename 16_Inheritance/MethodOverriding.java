c922d50873ce75841db81140d2cf6803c2eecdbe
Ramireddy Chandra sekhar reddy <ramireddychandu77@gmail.com>
2022-12-05 13:27:40 +0530

    Core Java OOPS Concept

---
+++ 16_Inheritance/MethodOverriding.java
@@ -0,0 +1,26 @@
+
+class Super
+{
+    public void display()
+    {
+        System.out.println("Super Display");
+    }
+}
+
+class Sub extends Super
+{
+    public void display()
+    {
+        System.out.println("Sub Display");
+    }
+}
+
+public class MethodOverriding
+{
+    public static void main(String[] args) 
+    {
+        Super s=new Sub();
+        s.display();
+          
+    }    
+}
